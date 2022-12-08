49160a41f4db2790601daca13a6005d32577f597
Gaein nidb <mail@gaein.cn>
2022-12-08 14:45:27 +0000

    Use getopt to parse arguments

--- main.c
+++ main.c
@@ -2,12 +2,16 @@
 #include "mac.h"
 #include "utils.h"
 #include "minieap.h"
+#include <ctype.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <arpa/inet.h>
 #include <time.h>
 #include <unistd.h>
 #include <signal.h>
+
+#define HELP_INFO "Try 'automac -h' for more information."
 
 void sendLoopInvoke(unsigned int seq, unsigned int dataSize) {
     printf("ping success, seq=%d, re %d bytes\n", seq, dataSize);
@@ -20,16 +24,67 @@ void interruptHandler(int dummy) {
 }
 
 int main(int argc, char *argv[]) {
-    if (argc < 4) {
-        printf("Usage: automac <interfaceName> <pingAddress> <command>\n");
-        exit(EXIT_SUCCESS);
+    bool cflag = false;
+    char *ifName = NULL;
+    char *ipAddress = NULL;
+    char *execCommand = NULL;
+
+    // no argument
+    if (argc < 1) {
+        fprintf(stdout, "Usage: automac -a <IP> -n <IF> [options...]\n"HELP_INFO);
+        exit(EXIT_FAILURE);
     }
 
-    printf("v1.2.20221015-1806\n");
+    // parse arguments
+    char opt;
+    while ((opt = (char) getopt(argc, argv, "hvn:a:c:")) != EOF) {
+        switch (opt) {
+            case 'h':
+                // h for help
+                printf("Usage: automac -a <IP> -n <IF> [options...]\n"
+                       " -a <address>   IP address to test internet status\n"
+                       " -n <if name>   Interface name to change MAC address\n"
+                       " -c <command>   Execute a command after changed success\n"
+                       " -h             Get help for commands\n"
+                       " -v             Show version number and quit");
+                exit(EXIT_SUCCESS);
+            case 'v':
+                // v for version
+                printf("automac v1.2.1-2022.12.08");
+                exit(EXIT_SUCCESS);
 
-    const char *ifName = argv[1];
-    const char *ipAddress = argv[2];
-    const char *command = argv[3];
+            case 'n':
+                // n for network device
+                ifName = optarg;
+                break;
+            case 'a':
+                // a for address
+                ipAddress = optarg;
+                break;
+            case 'c':
+                // c for command
+                cflag = true;
+                execCommand = optarg;
+                break;
+            case '?':
+                // ? for unmatched argument
+                if (optopt == 'n' || optopt == 'a')
+                    fprintf(stderr,
+                            "automac: option requires an argument -- '%c'\n" HELP_INFO,
+                            optopt);
+                else if (isprint(optopt))
+                    fprintf(stderr,
+                            "automac: invalid option -- '%c'\n" HELP_INFO,
+                            optopt);
+                else
+                    fprintf(stderr,
+                            "automac: invalid option --'0x%x'\n" HELP_INFO,
+                            optopt);
+                exit(EXIT_FAILURE);
+            default:
+                abort();
+        }
+    }
 
     if (!MAC_init()) {
         fprintf(stderr, "Can not Init MAC, exit.\n");
@@ -75,17 +130,18 @@ int main(int argc, char *argv[]) {
             MAC_setInterface(ifName, macAddress);
             MAC_print(macAddress);
 
-            printf("Restart interface.\n");
             MAC_restartInterface(ifName);
 
-            printf("Restart miniEAP, command: %s\n", command);
-            MINIEAP_restart(command);
+            // MINIEAP_restart(execCommand);
+            if (cflag) {
+                system(execCommand);
+            }
 
-            printf("miniEAP restarted, waiting 30s for next loop...\n");
+            fprintf(stdout, "Waiting 30s for next loop...\n");
             sleep(20);
         }
 
-        printf("Waiting 10s for next loop...\n");
+        fprintf(stdout, "Waiting 10s for next loop...\n");
         sleep(10);
     }
 }
